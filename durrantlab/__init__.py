65fabdd737c0c3f2edc489e69e9d5784153634fa
DaniDelHoyo <danieldelhoyog@gmail.com>
2023-06-14 16:22:57 +0200

    DeepFrag install

--- durrantlab/__init__.py
+++ durrantlab/__init__.py
@@ -54,10 +54,9 @@ class Plugin(pwchem.Plugin):
     def addDeepFragPackage(cls, env, default=False):
         DFRAG_INSTALLED = 'deepFrag_installed'
         dFragCommands = 'git clone {} && cd deepfrag && '.format(cls.getDeepFragGithub())
-        dFragCommands += 'conda create -y -n {} && '\
+        dFragCommands += 'conda create -y -n {} -c fastai --file requirements.txt prody=1.11 && '\
             .format(cls.getEnvName(DFRAG_DIC))
-        dFragCommands += '{} && conda config --env --add channels pytorch && conda config --env --add channels fastai && '.format(cls.getEnvActivationCommand(DFRAG_DIC))
-        dFragCommands += 'conda install --file requirements.txt prody=1.11 && pip install pyparsing==2.4.7 && '
+        dFragCommands += '{} && pip install pyparsing==2.4.7 && '.format(cls.getEnvActivationCommand(DFRAG_DIC))
         dFragCommands += "mkdir .store && wget {} -P .store && wget {} -O DFModel.zip && " \
                          "unzip DFModel.zip -d .store/model && ".\
             format(cls.getDeepFragFingerprints(), cls.getDeepFragModel())
